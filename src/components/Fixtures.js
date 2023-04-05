66eb136fb4859123db63b795c472f3d3a8e1930c
Suhho Lee <suhholeemichael@gmail.com>
2023-04-05 13:52:22 +0900

    test deployment

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -46,7 +46,7 @@ const Fixtures = () => {
         }
         if (currentMonth < 7) {
           fullDateString = `${currentYear}-0${currentMonth + 1}-${currentDate}`
-          const { data: { matches } } = await authenticated.get(`/competitions/2021/matches?season=${currentYear - 1}&dateTo=${fullDateString}&dateFrom=${fullDateString}`)
+          const { data: { matches } } = await authenticated.get(`/api/competitions/2021/matches?season=${currentYear - 1}&dateTo=${fullDateString}&dateFrom=${fullDateString}`)
           setFixtures(matches)
         } else if (currentMonth >= 7) {
           if (currentMonth >= 9) {
